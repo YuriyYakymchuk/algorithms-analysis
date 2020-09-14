09fed9e525f2b2157cf1acac9fc0642a859b68f6
Yuriy Yakymchuk <yakimchuk1@gmail.com>
2020-09-14 17:03:24 +0300

    Implemented merge sort algorithm

--- src/main/java/com/yuriy/algorithms/Main.java
+++ src/main/java/com/yuriy/algorithms/Main.java
@@ -2,13 +2,50 @@ package com.yuriy.algorithms;
 
 import com.yuriy.algorithms.sort.SortUtils;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
 
+    private final static String UNSORTED_ARRAY_FILE = "C:\\Git\\BEST\\algorithms-analysis\\src\\main\\resources\\UnsortedArray.txt";
+    private final static String BUBBLE_SORT_FILE = "C:\\Git\\BEST\\algorithms-analysis\\src\\main\\resources\\BubbleSort.txt";
+    private final static String MERGE_SORT_FILE = "C:\\Git\\BEST\\algorithms-analysis\\src\\main\\resources\\MergeSort.txt";
+    private final static String QUICK_SORT_FILE = "C:\\Git\\BEST\\algorithms-analysis\\src\\main\\resources\\QuickSort.txt";
+
     public static void main(String[] args) {
-        Integer[] array = new Integer[]{3, 1, 0, 4, 2, 1, 10, 7};
-        SortUtils.bubbleSort(array);
-        for (Integer i: array) {
-            System.out.print(i + " ");
+        Integer[] bubbleSortArray = readFile();
+        long start = System.currentTimeMillis();
+        SortUtils.bubbleSort(bubbleSortArray);
+        System.out.println(String.format("Bubble sort: %d ms", System.currentTimeMillis() - start));
+        writeToFile(BUBBLE_SORT_FILE, bubbleSortArray);
+
+        Integer[] mergeSortArray = readFile();
+        start = System.currentTimeMillis();
+        SortUtils.mergeSort(mergeSortArray);
+        System.out.println(String.format("Merge sort: %d ms", System.currentTimeMillis() - start));
+        writeToFile(MERGE_SORT_FILE, mergeSortArray);
+    }
+
+    private static void writeToFile (String fileName, Integer[] array) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));){
+            for (Integer number : array) {
+                writer.write(number + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Failed to write to the file.");
         }
+    }
+
+    private static Integer[] readFile() {
+        Integer[] array = new Integer[1000000];
+        int i = 0;
+        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(UNSORTED_ARRAY_FILE)))){
+            while (scanner.hasNext() && i < 1000000) {
+                array[i++] = scanner.nextInt();
+            }
+        } catch (Exception e) {
+            System.out.println("Failed to read from the file.");
+        }
+        return array;
     }
 }
--- src/main/java/com/yuriy/algorithms/sort/SortUtils.java
+++ src/main/java/com/yuriy/algorithms/sort/SortUtils.java
@@ -2,6 +2,44 @@ package com.yuriy.algorithms.sort;
 
 public class SortUtils {
 
+    public static void mergeSort(Comparable[] array) {
+        Comparable[] aux = new Comparable[array.length];
+        mergeSort(array, aux, 0, array.length - 1);
+    }
+
+    private static void mergeSort(Comparable[] array, Comparable[] aux, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int mid = (start + end) / 2;
+        mergeSort(array, aux, start, mid);
+        mergeSort(array, aux, mid + 1, end);
+       /* if (array[mid + 1].compareTo(array[mid]) >= 0) {
+            return;
+        }*/
+        merge(array, aux, start, end);
+    }
+
+    private static void merge(Comparable[] array, Comparable[] aux, int start, int end) {
+        int mid = (start + end) / 2;
+        for (int i = start; i <= end; i++) {
+            aux[i] = array[i];
+        }
+        int i = start;
+        int j = mid + 1;
+        for (int k = start; k <= end; k++) {
+            if (i > mid) {
+                array[k] = aux[j++];
+            } else if (j > end) {
+                array[k] = aux[i++];
+            } else if (aux[i].compareTo(aux[j]) < 0) {
+                array[k] = aux[i++];
+            } else {
+                array[k] = aux[j++];
+            }
+        }
+    }
+
     public static void bubbleSort(Comparable[] array) {
         if (array == null) {
             return;
