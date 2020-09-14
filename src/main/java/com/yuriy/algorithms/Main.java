a21fdbff93d86872a1296e694e7b0ba7d7d97f80
Yuriy Yakymchuk <yakimchuk1@gmail.com>
2020-09-14 21:36:27 +0300

    Implemented quick sort algorithm

--- src/main/java/com/yuriy/algorithms/Main.java
+++ src/main/java/com/yuriy/algorithms/Main.java
@@ -3,31 +3,41 @@ package com.yuriy.algorithms;
 import com.yuriy.algorithms.sort.SortUtils;
 
 import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main {
 
-    private final static String UNSORTED_ARRAY_FILE = "C:\\Git\\BEST\\algorithms-analysis\\src\\main\\resources\\UnsortedArray.txt";
-    private final static String BUBBLE_SORT_FILE = "C:\\Git\\BEST\\algorithms-analysis\\src\\main\\resources\\BubbleSort.txt";
-    private final static String MERGE_SORT_FILE = "C:\\Git\\BEST\\algorithms-analysis\\src\\main\\resources\\MergeSort.txt";
-    private final static String QUICK_SORT_FILE = "C:\\Git\\BEST\\algorithms-analysis\\src\\main\\resources\\QuickSort.txt";
+    private final static Path SOURCE_PATH = Paths.get(Main.class.getResource("/").getPath());
+    private final static String UNSORTED_ARRAY_FILE = "/UnsortedArray.txt";
+    private final static String BUBBLE_SORT_FILE = "/BubbleSort.txt";
+    private final static String MERGE_SORT_FILE = "/MergeSort.txt";
+    private final static String QUICK_SORT_FILE = "/QuickSort.txt";
 
     public static void main(String[] args) {
-        Integer[] bubbleSortArray = readFile();
-        long start = System.currentTimeMillis();
-        SortUtils.bubbleSort(bubbleSortArray);
-        System.out.println(String.format("Bubble sort: %d ms", System.currentTimeMillis() - start));
-        writeToFile(BUBBLE_SORT_FILE, bubbleSortArray);
 
-        Integer[] mergeSortArray = readFile();
+        Integer[] bubbleSortArray = readFile(SOURCE_PATH.toAbsolutePath() + UNSORTED_ARRAY_FILE);
+        long start = System.currentTimeMillis();
+      //  SortUtils.bubbleSort(bubbleSortArray);
+        System.out.println(String.format("Bubble sort: %d ms", System.currentTimeMillis() - start));
+        writeToFile(SOURCE_PATH.toAbsolutePath() + BUBBLE_SORT_FILE, bubbleSortArray);
+
+        Integer[] mergeSortArray = readFile(SOURCE_PATH.toAbsolutePath() + UNSORTED_ARRAY_FILE);
         start = System.currentTimeMillis();
         SortUtils.mergeSort(mergeSortArray);
         System.out.println(String.format("Merge sort: %d ms", System.currentTimeMillis() - start));
-        writeToFile(MERGE_SORT_FILE, mergeSortArray);
+        writeToFile(SOURCE_PATH.toAbsolutePath() + MERGE_SORT_FILE, mergeSortArray);
+
+        Integer[] quickSortArray = readFile(SOURCE_PATH.toAbsolutePath() + UNSORTED_ARRAY_FILE);
+        start = System.currentTimeMillis();
+        SortUtils.quickSort(quickSortArray);
+        System.out.println(String.format("Quick sort: %d ms", System.currentTimeMillis() - start));
+        writeToFile(SOURCE_PATH.toAbsolutePath() + QUICK_SORT_FILE, quickSortArray);
     }
 
     private static void writeToFile (String fileName, Integer[] array) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));){
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, false));){
             for (Integer number : array) {
                 writer.write(number + "\n");
             }
@@ -36,10 +46,10 @@ public class Main {
         }
     }
 
-    private static Integer[] readFile() {
+    private static Integer[] readFile(String fileName) {
         Integer[] array = new Integer[1000000];
         int i = 0;
-        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(UNSORTED_ARRAY_FILE)))){
+        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(fileName)))){
             while (scanner.hasNext() && i < 1000000) {
                 array[i++] = scanner.nextInt();
             }
--- src/main/java/com/yuriy/algorithms/sort/SortUtils.java
+++ src/main/java/com/yuriy/algorithms/sort/SortUtils.java
@@ -2,44 +2,6 @@ package com.yuriy.algorithms.sort;
 
 public class SortUtils {
 
-    public static void mergeSort(Comparable[] array) {
-        Comparable[] aux = new Comparable[array.length];
-        mergeSort(array, aux, 0, array.length - 1);
-    }
-
-    private static void mergeSort(Comparable[] array, Comparable[] aux, int start, int end) {
-        if (start >= end) {
-            return;
-        }
-        int mid = (start + end) / 2;
-        mergeSort(array, aux, start, mid);
-        mergeSort(array, aux, mid + 1, end);
-        if (array[mid + 1].compareTo(array[mid]) >= 0) {
-            return;
-        }
-        merge(array, aux, start, end);
-    }
-
-    private static void merge(Comparable[] array, Comparable[] aux, int start, int end) {
-        int mid = (start + end) / 2;
-        for (int i = start; i <= end; i++) {
-            aux[i] = array[i];
-        }
-        int i = start;
-        int j = mid + 1;
-        for (int k = start; k <= end; k++) {
-            if (i > mid) {
-                array[k] = aux[j++];
-            } else if (j > end) {
-                array[k] = aux[i++];
-            } else if (aux[i].compareTo(aux[j]) < 0) {
-                array[k] = aux[i++];
-            } else {
-                array[k] = aux[j++];
-            }
-        }
-    }
-
     public static void bubbleSort(Comparable[] array) {
         if (array == null) {
             return;
@@ -57,5 +19,72 @@ public class SortUtils {
         Comparable temp = array[i];
         array[i] = array[j];
         array[j] = temp;
+    }
+
+    public static void mergeSort(Comparable[] array) {
+        Comparable[] tempArray = new Comparable[array.length];
+        mergeSort(array, tempArray, 0, array.length - 1);
+    }
+
+    private static void mergeSort(Comparable[] array, Comparable[] tempArray, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int mid = (start + end) / 2;
+        mergeSort(array, tempArray, start, mid);
+        mergeSort(array, tempArray, mid + 1, end);
+        if (array[mid + 1].compareTo(array[mid]) >= 0) {
+            return;
+        }
+        merge(array, tempArray, start, end);
+    }
+
+    private static void merge(Comparable[] array, Comparable[] tempArray, int start, int end) {
+        int mid = (start + end) / 2;
+        for (int i = start; i <= mid; i++) {
+            tempArray[i] = array[i];
+        }
+        int i = start;
+        int j = mid + 1;
+        for (int k = start; k <= end; k++) {
+            if (i > mid) {
+                array[k] = array[j++];
+            } else if (j > end) {
+                array[k] = tempArray[i++];
+            } else if (tempArray[i].compareTo(array[j]) < 0) {
+                array[k] = tempArray[i++];
+            } else {
+                array[k] = array[j++];
+            }
+        }
+    }
+
+    public static void quickSort(Comparable[] array) {
+        threeWayQuickSort(array, 0, array.length - 1);
+    }
+
+    private static void threeWayQuickSort(Comparable[] array, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        Comparable v = array[start];
+        int i = start + 1;
+        int lessThanIndex = start;
+        int greaterThanIndex = end;
+        while (i <= greaterThanIndex) {
+            int compareResult = array[i].compareTo(v);
+            if (compareResult < 0) {
+                swap(array, lessThanIndex, i);
+                lessThanIndex++;
+                i++;
+            } else if (compareResult > 0) {
+                swap(array, greaterThanIndex, i);
+                greaterThanIndex--;
+            } else {
+                i++;
+            }
+        }
+        threeWayQuickSort(array, start, lessThanIndex - 1);
+        threeWayQuickSort(array, greaterThanIndex + 1, end);
     }
 }
