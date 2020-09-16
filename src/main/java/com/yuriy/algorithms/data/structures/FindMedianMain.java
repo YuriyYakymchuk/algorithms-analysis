b561017c41db03786648f66e96c45a83102515af
Yuriy Yakymchuk <yakimchuk1@gmail.com>
2020-09-16 09:43:13 +0300

    Implemented an example for MedianFinder

--- src/main/java/com/yuriy/algorithms/data/structures/FindMedianMain.java
+++ src/main/java/com/yuriy/algorithms/data/structures/FindMedianMain.java
@@ -1,17 +1,45 @@
 package com.yuriy.algorithms.data.structures;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class FindMedianMain {
 
     public static void main(String[] args) {
         MedianFinder medianFinder = new MedianFinder();
-        medianFinder.addNum(1);
-        System.out.println(medianFinder.findMedian());
-        medianFinder.addNum(3);
-        System.out.println(medianFinder.findMedian());
-        medianFinder.addNum(2);
-        System.out.println(medianFinder.findMedian());
-        medianFinder.addNum(5);
-        System.out.println(medianFinder.findMedian());
+        Random random = new Random();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            int number = random.nextInt(10);
+            list.add(number);
+            medianFinder.addNum(number);
+        }
+        Collections.sort(list);
+        printList(list);
+        System.out.println("Median: " + medianFinder.findMedian());
+
+        int number = random.nextInt(10);
+        list.add(number);
+        medianFinder.addNum(number);
+        Collections.sort(list);
+        printList(list);
+        System.out.println("Median: " + medianFinder.findMedian());
+
+        number = random.nextInt(10);
+        list.add(number);
+        medianFinder.addNum(number);
+        Collections.sort(list);
+        printList(list);
+        System.out.println("Median: " + medianFinder.findMedian());
+    }
+
+    private static void printList(List<Integer> list) {
+        for (Integer number : list) {
+            System.out.print(number + " ");
+        }
+        System.out.println("");
     }
 
 }
